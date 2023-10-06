706de806c11c1cd32831a219179893ae361154fb
emanuelbispoc <emanuelbispoc@gmail.com>
2023-10-06 10:31:54 -0300

    refactor: removendo importacao inutilizada

--- votacao/src/main/java/com/assembleia/app/votacao/service/SessaoService.java
+++ votacao/src/main/java/com/assembleia/app/votacao/service/SessaoService.java
@@ -2,7 +2,6 @@ package com.assembleia.app.votacao.service;
 
 import com.assembleia.app.votacao.dto.request.SessaoRequest;
 import com.assembleia.app.votacao.dto.response.SessaoResponse;
-import com.assembleia.app.votacao.model.Sessao;
 
 public interface SessaoService {
     SessaoResponse salvar(SessaoRequest request);
