7e3aaf5dade99ddf991a9724d33592588865c814
emanuelbispoc <emanuelbispoc@gmail.com>
2023-10-06 10:47:00 -0300

    refactor: corrigindo mapeamento resposta nome relator

--- votacao/src/main/java/com/assembleia/app/votacao/mapper/SessaoMapper.java
+++ votacao/src/main/java/com/assembleia/app/votacao/mapper/SessaoMapper.java
@@ -3,8 +3,10 @@ package com.assembleia.app.votacao.mapper;
 import com.assembleia.app.votacao.dto.response.SessaoResponse;
 import com.assembleia.app.votacao.model.Sessao;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SessaoMapper {
+    @Mapping(source = "pauta.relator.nome", target = "pauta.relatorNome")
     SessaoResponse modelToResponse(Sessao sessao);
 }
