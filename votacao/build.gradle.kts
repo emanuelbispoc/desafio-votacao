4e212efe335eed9ed02c1675a82d6ebe9b69e45e
emanuelbispoc <emanuelbispoc@gmail.com>
2023-12-13 11:25:21 -0300

    chore: adicionando dependencias para testes da controller

--- votacao/build.gradle.kts
+++ votacao/build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 	runtimeOnly ("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.rest-assured:rest-assured:5.4.0")
+
 }
 
 tasks.withType<Test> {
