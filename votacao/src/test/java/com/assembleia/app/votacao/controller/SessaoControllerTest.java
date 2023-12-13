16d4d4db3fa27c27c8ab3c93c30d6d7c223d399d
emanuelbispoc <emanuelbispoc@gmail.com>
2023-12-13 11:27:31 -0300

    test: testando controller de sessoes

---
+++ votacao/src/test/java/com/assembleia/app/votacao/controller/SessaoControllerTest.java
@@ -0,0 +1,142 @@
+package com.assembleia.app.votacao.controller;
+
+import com.assembleia.app.votacao.dto.request.VotoRequest;
+import com.assembleia.app.votacao.enums.SessaoStatus;
+import com.assembleia.app.votacao.enums.Voto;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import static com.assembleia.app.votacao.SqlProvider.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@SqlGroup({
+        @Sql(scripts = INSERT_ASSOCIADOS, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(scripts = INSERT_SESSOES, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(scripts = CLEAR_DATABASE, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SessaoControllerTest {
+
+    @LocalServerPort
+    private int port;
+    private static final String BASE_URL =  "/v1/sessoes";
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = this.port;
+    }
+
+    @Test
+    void deveReceberVotoComSucesso() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+        .when()
+                .body(new VotoRequest("51682883086", Voto.NAO))
+                .post(BASE_URL + "/1/registra-voto")
+        .then()
+                .assertThat()
+                .statusCode(200)
+                .body(
+                        "sessaoId", is(1),
+                        "totalVotosSim", is(0),
+                        "totalVotosNao", is(2)
+                );
+    }
+
+    @Test
+    void deveRetornarSessaoPorIdComStatusAprovado() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .when()
+                .get(BASE_URL + "/2")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(
+                        "id", is(2),
+                        "votosSim", is(2),
+                        "votosNao", is(1),
+                        "status",  is(SessaoStatus.PAUTA_APROVADA.toString()),
+                        "dataInicio", is("2023-12-12T10:00:00"),
+                        "dataFim", is("2023-12-12T15:00:00")
+                );
+    }
+
+    @Test
+    void deveRetornarSessaoPorIdComStatusReprovado() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .when()
+                .get(BASE_URL + "/3")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(
+                        "id", is(3),
+                        "votosSim", is(1),
+                        "votosNao", is(2),
+                        "status",  is(SessaoStatus.PAUTA_REPROVADA.toString()),
+                        "dataInicio", is("2023-12-12T14:00:00"),
+                        "dataFim", is("2023-12-13T09:00:00")
+                );
+    }
+
+    @Test
+    void deveRetornarErroParaAssociadoQueJaVotou() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .when()
+                .body(new VotoRequest("85903326323", Voto.SIM))
+                .post(BASE_URL + "/1/registra-voto")
+                .then()
+                .assertThat()
+                .statusCode(422)
+                .body(
+                        "descricao", is("O associado já registrou voto na sessão.")
+                );
+    }
+
+    @Test
+    void deveRetornarErroAoTentarReceberVotoAposEncerramento() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .when()
+                .body(new VotoRequest("51682883086", Voto.SIM))
+                .post(BASE_URL + "/2/registra-voto")
+                .then()
+                .assertThat()
+                .statusCode(422)
+                .body(
+                        "descricao", is("Sessão não está em andamento.")
+                );
+    }
+
+    @Test
+    void deveRetornarErroAoTentarReceberVotoAssociadoNaoRegistrado() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .when()
+                .body(new VotoRequest("20425656870", Voto.SIM))
+                .post(BASE_URL + "/1/registra-voto")
+                .then()
+                .assertThat()
+                .statusCode(422)
+                .body(
+                        "descricao", is("Associado não encontrado.")
+                );
+    }
+
+}
