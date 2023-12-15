62ed2f837a73f3e5ac6efd4be266b93e978d43bb
emanuelbispoc <emanuelbispoc@gmail.com>
2023-12-15 18:28:49 -0300

    chore: adicionando, configurando: wiremock, feignclient

--- votacao/build.gradle.kts
+++ votacao/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("org.flywaydb:flyway-core")
 	implementation ("org.mapstruct:mapstruct:1.5.5.Final")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -32,7 +33,13 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.rest-assured:rest-assured:5.4.0")
+	testImplementation("com.github.tomakehurst:wiremock:3.0.1")
+}
 
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2022.0.4")
+	}
 }
 
 tasks.withType<Test> {
