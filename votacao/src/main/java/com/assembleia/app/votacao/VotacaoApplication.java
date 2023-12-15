5b5a1363c77e7657054f348f00c09a2c5f158abf
emanuelbispoc <emanuelbispoc@gmail.com>
2023-12-15 18:31:30 -0300

    chore: ativando feign client

--- votacao/src/main/java/com/assembleia/app/votacao/VotacaoApplication.java
+++ votacao/src/main/java/com/assembleia/app/votacao/VotacaoApplication.java
@@ -2,7 +2,9 @@ package com.assembleia.app.votacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class VotacaoApplication {
 
