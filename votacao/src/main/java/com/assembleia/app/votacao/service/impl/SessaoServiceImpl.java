70c103d9980c076c485c6033749de03a52d1ae60
emanuelbispoc <emanuelbispoc@gmail.com>
2023-12-15 18:33:00 -0300

    feat: validando situacao do cpf do associado antes de votar

--- votacao/src/main/java/com/assembleia/app/votacao/service/impl/SessaoServiceImpl.java
+++ votacao/src/main/java/com/assembleia/app/votacao/service/impl/SessaoServiceImpl.java
@@ -6,6 +6,7 @@ import com.assembleia.app.votacao.dto.response.SessaoCriadaResponse;
 import com.assembleia.app.votacao.dto.response.SessaoResponse;
 import com.assembleia.app.votacao.dto.response.VotoSessaoResponse;
 import com.assembleia.app.votacao.enums.SessaoStatus;
+import com.assembleia.app.votacao.enums.SituacaoCpf;
 import com.assembleia.app.votacao.exception.NotFoundException;
 import com.assembleia.app.votacao.exception.UnprocessableEntityException;
 import com.assembleia.app.votacao.mapper.SessaoMapper;
@@ -15,6 +16,7 @@ import com.assembleia.app.votacao.model.Sessao;
 import com.assembleia.app.votacao.model.VotoSessao;
 import com.assembleia.app.votacao.repository.SessaoRepository;
 import com.assembleia.app.votacao.service.AssociadoService;
+import com.assembleia.app.votacao.service.CpfService;
 import com.assembleia.app.votacao.service.PautaService;
 import com.assembleia.app.votacao.service.SessaoService;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Service
 public class SessaoServiceImpl implements SessaoService {
+    private final CpfService cpfService;
     private final PautaService pautaService;
     private final AssociadoService associadoService;
     private final SessaoRepository repository;
@@ -50,7 +53,7 @@ public class SessaoServiceImpl implements SessaoService {
     @Override
     public VotoSessaoResponse receberVoto(Long sessaoId, VotoRequest request) {
         Associado associado = associadoService.buscarPorCpf(request.associadoCpf());
-        Sessao sessao = verificaSeSessaoPodeReceberVotos(sessaoId, associado.getId());
+        Sessao sessao = verificaSeSessaoPodeReceberVotos(sessaoId, associado);
 
         sessao.adicionaVoto(new VotoSessao(request.voto(), associado));
         Sessao sessaoAtualizada = repository.save(sessao);
@@ -62,20 +65,24 @@ public class SessaoServiceImpl implements SessaoService {
                 .orElseThrow(() -> new NotFoundException("Sessão não encontrada."));
     }
 
-    private Sessao verificaSeSessaoPodeReceberVotos(Long sessaoId, Long associadoId) {
+    private Sessao verificaSeSessaoPodeReceberVotos(Long sessaoId, Associado associado) {
         Sessao sessao = verificaSeSessaoExistePorId(sessaoId);
 
         if(sessao.getStatus() != SessaoStatus.EM_ANDAMENTO) {
             throw new UnprocessableEntityException("Sessão não está em andamento.");
         }
 
-        verificaSePodeReceberVotoAssociado(sessao.getId(), associadoId);
+        verificaSePodeReceberVotoAssociado(sessao.getId(), associado);
         return sessao;
     }
 
-    private void verificaSePodeReceberVotoAssociado(Long sessaoId, Long associadoId) {
-        if (repository.existsByVotosIdSessaoIdAndVotosIdAssociadoId(sessaoId, associadoId)) {
+    private void verificaSePodeReceberVotoAssociado(Long sessaoId, Associado associado) {
+        if (repository.existsByVotosIdSessaoIdAndVotosIdAssociadoId(sessaoId, associado.getId())) {
             throw new UnprocessableEntityException("O associado já registrou voto na sessão.");
+        }
+
+        if(cpfService.verificaSituacao(associado.getCpf()).status() == SituacaoCpf.IRREGULAR) {
+            throw new UnprocessableEntityException("UNABLE_TO_VOTE");
         }
     }
 }
