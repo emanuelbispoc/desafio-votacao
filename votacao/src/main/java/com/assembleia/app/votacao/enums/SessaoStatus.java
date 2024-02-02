fdb45fd4cea2d51b5c59df76dec3826d39224f84
emanuelbispoc <emanuelbispoc@gmail.com>
2024-02-02 18:06:43 -0300

    refactor: ajustando enumerador de status da sessao.

--- votacao/src/main/java/com/assembleia/app/votacao/enums/SessaoStatus.java
+++
@@ -1,8 +0,0 @@
-package com.assembleia.app.votacao.enums;
-
-public enum SessaoStatus {
-    EM_ANDAMENTO,
-    EMPATE,
-    PAUTA_APROVADA,
-    PAUTA_REPROVADA
-}
---
+++ votacao/src/main/java/com/assembleia/app/votacao/model/enums/SessaoStatus.java
@@ -0,0 +1,6 @@
+package com.assembleia.app.votacao.model.enums;
+
+public enum SessaoStatus {
+    EM_ANDAMENTO,
+    FINALIZADO,
+}
