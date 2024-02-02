aa403d447791b4420f6f6f92a120ebdc93a03058
emanuelbispoc <emanuelbispoc@gmail.com>
2024-02-02 18:16:27 -0300

    refactor: corrigindo validacao de cpf

--- votacao/src/main/java/com/assembleia/app/votacao/client/CpfClient.java
+++ votacao/src/main/java/com/assembleia/app/votacao/client/CpfClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "validadorCpf", url = "${cpfvalidador.api.url}")
 public interface CpfClient {
     @GetMapping(value = "/{cpf}")
-    void valida(@PathVariable String cpf);
+    void verificarSeExiste(@PathVariable String cpf);
 
     @GetMapping(value = "/{cpf}/situacao")
     CpfValidacaoResponse buscar(@PathVariable String cpf);
